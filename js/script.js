dc05d0d8d0f2542a6cd43cd2f67e5c3f356fe504
danilopiccinni <danilopiccinni@gmail.com>
2023-02-21 13:10:14 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+let nomeutente;
+let cognomeutente;
+let colorepreferitoutente;
+let passwordgenerata;
+
+nomeutente = prompt('Inserisci il tuo nome')
+
+cognomeutente = prompt('nserisci il tuo cognome')
+
+colorepreferitoutente = prompt('Inserisci il tuo colore preferito')
+
+document.writeln( passwordgenerata = nomeutente + cognomeutente + colorepreferitoutente + '22')
