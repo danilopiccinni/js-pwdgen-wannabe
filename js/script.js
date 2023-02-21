9189804d15a033ad1cd871cac82645dd9da28338
danilopiccinni <danilopiccinni@gmail.com>
2023-02-21 13:20:14 +0100

    prova alternativa funzionante (curiosità)

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ let cognomeutente;
 let colorepreferitoutente;
 let passwordgenerata;
 
-nomeutente = prompt('Inserisci il tuo nome')
+// nomeutente = prompt('Inserisci il tuo nome')
 
-cognomeutente = prompt('nserisci il tuo cognome')
+// cognomeutente = prompt('nserisci il tuo cognome')
 
-colorepreferitoutente = prompt('Inserisci il tuo colore preferito')
+// colorepreferitoutente = prompt('Inserisci il tuo colore preferito')
 
-document.writeln( passwordgenerata = nomeutente + cognomeutente + colorepreferitoutente + '22')
+// document.writeln( passwordgenerata = nomeutente + cognomeutente + colorepreferitoutente + '22')
+
+document.writeln ( 
+    passwordgenerata = (
+                        (nomeutente = prompt('Inserisci il tuo nome')) +
+                        (cognomeutente = prompt('nserisci il tuo cognome')) +
+                        (colorepreferitoutente = prompt('Inserisci il tuo colore preferito')) +
+                        '22'
+                            )
+)
